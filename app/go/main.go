d1e4371d9a85f6ee7ccc9f917804bc910d2e8fa0
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-10-14 00:42:29 +0900

    ST_GeomFromText は (latitude, longitude) の順?

--- app/go/main.go
+++ app/go/main.go
@@ -961,7 +961,7 @@ func (cs Coordinates) getBoundingBox() BoundingBox {
 func (cs Coordinates) coordinatesToText() string {
 	points := make([]string, 0, len(cs.Coordinates))
 	for _, c := range cs.Coordinates {
-		points = append(points, fmt.Sprintf("%f %f", c.Longitude, c.Latitude))
+		points = append(points, fmt.Sprintf("%f %f", c.Latitude, c.Longitude))
 	}
 	return fmt.Sprintf("'POLYGON((%s))'", strings.Join(points, ","))
 }
