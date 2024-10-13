a3768d73f3d9f2cf926e126e69e157f123b2a464
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-10-13 22:09:30 +0900

    generated column を先に作る

--- app/mysql/db/3_Migration.sql
+++ app/mysql/db/3_Migration.sql
@@ -1,4 +1,8 @@
 ALTER TABLE `chair`
+    ADD COLUMN `in_stock` BOOLEAN
+        GENERATED ALWAYS AS (`stock` > 0) STORED;
+
+ALTER TABLE `chair`
     ADD INDEX width_stock_popularity_id_idx (`width`, `stock`, `popularity` DESC, `id`);
 ALTER TABLE `chair`
     ADD INDEX height_stock_popularity_id_idx (`height`, `stock`, `popularity` DESC, `id`);
@@ -10,7 +14,3 @@ ALTER TABLE `estate`
     ADD INDEX door_width_popularity_id_idx (`door_width`, `popularity` DESC, `id`);
 ALTER TABLE `estate`
     ADD INDEX door_height_popularity_id_idx (`door_height`, `popularity` DESC, `id`);
-
-ALTER TABLE `chair`
-    ADD COLUMN `in_stock` BOOLEAN
-        GENERATED ALWAYS AS (`stock` > 0) STORED;
