439a81542b66e409339bbdce6a6d4b23ea59c65d
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-10-13 22:29:54 +0900

    index: (popularity, id)

--- app/mysql/db/3_Migration.sql
+++ app/mysql/db/3_Migration.sql
@@ -4,14 +4,8 @@ ALTER TABLE `chair`
         STORED NOT NULL;
 
 ALTER TABLE `chair`
-    ADD INDEX width_stock_popularity_id_idx (`width`, `stock`, `popularity` DESC, `id`);
-ALTER TABLE `chair`
-    ADD INDEX height_stock_popularity_id_idx (`height`, `stock`, `popularity` DESC, `id`);
-ALTER TABLE `chair`
-    ADD INDEX depth_stock_popularity_id_idx (`depth`, `stock`, `popularity` DESC, `id`);
+    ADD INDEX popularity_id_idx (`popularity` DESC, `id`);
 ALTER TABLE `chair`
     ADD INDEX in_stock_price_id (`in_stock`, `price`, `id`);
 ALTER TABLE `estate`
-    ADD INDEX door_width_popularity_id_idx (`door_width`, `popularity` DESC, `id`);
-ALTER TABLE `estate`
-    ADD INDEX door_height_popularity_id_idx (`door_height`, `popularity` DESC, `id`);
+    ADD INDEX popularity_id_idx (`popularity` DESC, `id`);
