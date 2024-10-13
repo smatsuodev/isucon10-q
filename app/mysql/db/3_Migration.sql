3fea313356983fbf748149e44a13f73c20cfbc80
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-10-13 21:50:28 +0900

    in_stock generated column を追加

--- app/mysql/db/3_Migration.sql
+++ app/mysql/db/3_Migration.sql
@@ -1,5 +1,16 @@
-ALTER TABLE `chair` ADD INDEX width_stock_popularity_id_idx (`width`, `stock`, `popularity` DESC, `id`);
-ALTER TABLE `chair` ADD INDEX height_stock_popularity_id_idx (`height`, `stock`, `popularity` DESC, `id`);
-ALTER TABLE `chair` ADD INDEX depth_stock_popularity_id_idx (`depth`, `stock`, `popularity` DESC, `id`);
-ALTER TABLE `estate` ADD INDEX door_width_popularity_id_idx (`door_width`, `popularity` DESC, `id`);
-ALTER TABLE `estate` ADD INDEX door_height_popularity_id_idx (`door_height`, `popularity` DESC, `id`);
+ALTER TABLE `chair`
+    ADD INDEX width_stock_popularity_id_idx (`width`, `stock`, `popularity` DESC, `id`);
+ALTER TABLE `chair`
+    ADD INDEX height_stock_popularity_id_idx (`height`, `stock`, `popularity` DESC, `id`);
+ALTER TABLE `chair`
+    ADD INDEX depth_stock_popularity_id_idx (`depth`, `stock`, `popularity` DESC, `id`);
+ALTER TABLE `chair`
+    ADD INDEX in_stock_price_id (`in_stock`, `price`, `id`);
+ALTER TABLE `estate`
+    ADD INDEX door_width_popularity_id_idx (`door_width`, `popularity` DESC, `id`);
+ALTER TABLE `estate`
+    ADD INDEX door_height_popularity_id_idx (`door_height`, `popularity` DESC, `id`);
+
+ALTER TABLE `chair`
+    ADD COLUMN `in_stock` BOOLEAN
+        GENERATED ALWAYS AS (`stock` > 0) STORED;
