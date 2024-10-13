fc54eaa5447c552a6fc8fcfa340b20977f8ef5d6
tkuramot <mabdman512@gmail.com>
2024-10-13 20:33:07 +0900

    migrationの修正

--- app/mysql/db/3_Migration.sql
+++ app/mysql/db/3_Migration.sql
@@ -1,6 +1,5 @@
-ALTER TABLE `isuumo.chair` ADD INDEX width_stock_idx (`width`, `stock`);
-ALTER TABLE `isuumo.chair` ADD INDEX depth_stock_idx (`depth`, `stock`);
-ALTER TABLE `isuumo.chair` ADD INDEX price_stock_idx (`price`, `stock`);
-ALTER TABLE `isuumo.estate` ADD INDEX door_height_idx (`door_height`);
-ALTER TABLE `isuumo.estate` ADD INDEX door_width_idx (`door_width`);
-ALTER TABLE `isuumo.estate` ADD INDEX rent_idx (`rent`);
+ALTER TABLE `chair` ADD INDEX width_stock_popularity_id_idx (`width`, `stock`, `popularity` DESC, `id`);
+ALTER TABLE `chair` ADD INDEX height_stock_popularity_id_idx (`height`, `stock`, `popularity` DESC, `id`);
+ALTER TABLE `chair` ADD INDEX depth_stock_popularity_id_idx (`depth`, `stock`, `popularity` DESC, `id`);
+ALTER TABLE `estate` ADD INDEX door_width_popularity_id_idx (`door_width`, `popularity` DESC, `id`);
+ALTER TABLE `estate` ADD INDEX door_height_popularity_id_idx (`door_height`, `popularity` DESC, `id`);
